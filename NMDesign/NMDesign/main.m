db67076e3de72f8014938e437cc6b6379a09d4e4
dahuang <huangzhaoqing@gameapp.net>
2017-07-10 11:27:28 +0800

    创建项目

---
+++ NMDesign/NMDesign/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  NMDesign
+//
+//  Created by idolplay-macpro on 2017/7/10.
+//  Copyright © 2017年 namunaka. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+  @autoreleasepool {
+      return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+  }
+}
